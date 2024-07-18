02463e5587cd3293c2d178719920a7e6377adfa1
OCHOMONG <admireplus@naver.com>
2024-07-19 01:45:17 +0900

    feat: 탭 기능 구현 analysis.js 페이지에 탭 기능 구현 완료

--- frontend/src/pages/analysis.js
+++ frontend/src/pages/analysis.js
@@ -1,9 +1,25 @@
+import { Nav } from 'react-bootstrap';
 
-
-function Analysis(){
-    return(
+function Analysis() {
+    return (
         <div>
-            탭 구현 페이지입니다.
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link0">Alignment</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link1">Gene</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link2">Mutation</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link3">Pathogenecity</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link4">Analyze</Nav.Link>
+                </Nav.Item>
+            </Nav>
         </div>
 
     );
