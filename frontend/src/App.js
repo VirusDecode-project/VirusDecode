274c59463f58e24e856bb668a321fb0273f92c0e
OCHOMONG <admireplus@naver.com>
2024-07-23 05:41:30 +0900

    새로고침 버그 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,31 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container } from 'react-bootstrap';
 import logo from './logo.png';//이거 퍼블릭 폴더에 넣기
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate, useLocation} from 'react-router-dom'
 
 import InputSeq from './pages/inputSeq.js'
 import Analysis from './pages/analysis.js'
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import historyIcon from './history.png';
 import editIcon from './edit.png';
 
 function App() {
 
   let navigate = useNavigate();
+  let location = useLocation();
 
   const [show, setShow] = useState(false);
   const [isHome, setIsHome] = useState(true);
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setIsHome(true);
+      setShow(false);
+    } else {
+      setIsHome(false);
+      setShow(true);
+    }
+  }, [location.pathname]);
 
 
 
@@ -23,15 +34,9 @@ function App() {
   const handleShow = () => setShow(true);
 
   const handleNavigate = (path) => {
-    if (path === '/') {
-      setIsHome(true);
-      setShow(false); // 홈으로 갈 때는 사이드바를 닫습니다.
-    } else {
-      setIsHome(false);
-      setShow(true); // 다른 페이지로 갈 때는 사이드바를 엽니다.
-    }
     navigate(path);
   }
+
 
   return (
     <div className="App">
@@ -51,6 +56,47 @@ function App() {
           <div>Reference1</div>
           <div>Reference2</div>
           <div>Reference3</div>
+          <div>Today</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <br />
+          <div>Previous 7 days</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <div>Reference3</div>
+          <div>Today</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <br />
+          <div>Previous 7 days</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <div>Reference3</div>
+          <div>Today</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <br />
+          <div>Previous 7 days</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <div>Reference3</div>
+          <div>Today</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <br />
+          <div>Previous 7 days</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <div>Reference3</div>
+          <div>Today</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <br />
+          <div>Previous 7 days</div>
+          <div>Reference1</div>
+          <div>Reference2</div>
+          <div>Reference3</div>
+
 
         </div>
       </div>
