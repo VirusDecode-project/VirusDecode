5c2e734bda1fb45f4795aaa4f74544cdd639c322
오성현 <henryosh@oseonghyeon-ui-MacBookPro.local>
2024-08-05 18:16:19 +0900

    feat: 로그인 가구현 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.png';
-import { Routes, Route, useNavigate, useLocation} from 'react-router-dom'
-import InputSeq from './pages/inputSeq.js'
-import Analysis from './pages/analysis.js'
-import { React, useState, useEffect } from 'react';
-import historyIcon from './history.png';
-import editIcon from './edit.png';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.png";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import InputSeq from "./pages/inputSeq.js";
+import Analysis from "./pages/analysis.js";
+import { React, useState, useEffect } from "react";
+import historyIcon from "./history.png";
+import editIcon from "./edit.png";
 
 function App() {
-
   let navigate = useNavigate();
   let location = useLocation();
 
   const [show, setShow] = useState(false);
   const [isHome, setIsHome] = useState(true);
-  
+  const [username, setUsername] = useState(null); // 사용자 이름 상태 추가
 
   useEffect(() => {
-    if (location.pathname === '/') {
+    if (location.pathname === "/") {
       setIsHome(true);
       setShow(false);
     } else {
@@ -27,23 +26,23 @@ function App() {
     }
   }, [location.pathname]);
 
-
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleNavigate = (path) => {
     navigate(path);
-  }
-
+  };
 
   return (
     <div className="App">
-
-      <div className={`sidebar ${show ? 'show' : ''}`}>
+      <div className={`sidebar ${show ? "show" : ""}`}>
         <div className="sidebar-header">
-
-          <img className="history-icon" src={historyIcon} onClick={handleClose} style={{ cursor: 'pointer' }} />
+          <img
+            className="history-icon"
+            src={historyIcon}
+            onClick={handleClose}
+            style={{ cursor: "pointer" }}
+          />
           <img src={editIcon} alt="Edit" className="edit-icon" />
         </div>
         <div className="sidebar-body">
@@ -75,54 +74,78 @@ function App() {
           <div>Referencea</div>
           <div>Referenceb</div>
           <div>Referencec</div>
-
-
-
         </div>
       </div>
 
-
-      <div className={`content-container ${show ? 'shrink' : ''}`}>
+      <div className={`content-container ${show ? "shrink" : ""}`}>
         <div className="header-bar">
+        <div className="header-left">
           {!show && !isHome && (
             <>
-
-              <img onClick={handleShow} style={{ cursor: 'pointer' }} src={historyIcon} alt="History" className="history-icon" />
+              <img
+                onClick={handleShow}
+                style={{ cursor: "pointer" }}
+                src={historyIcon}
+                alt="History"
+                className="history-icon"
+              />
               <img src={editIcon} alt="Edit" className="edit-icon" />
             </>
           )}
-          <span className='logo-text' onClick={() => { handleNavigate('/') }} style={{ cursor: 'pointer' }}>
-            {isHome &&
+          <span
+            className="logo-text"
+            onClick={() => {
+              handleNavigate("/");
+            }}
+            style={{ cursor: "pointer" }}
+          >
+            {isHome && (
               <img
                 alt="VirusDecode Logo"
                 src={logo}
                 width="30"
                 height="30"
                 className="d-inline-block align-top"
-                style={{ marginRight: '10px' }}
-              />}VirusDecode</span>
+                style={{ marginRight: "10px" }}
+              />
+            )}
+            VirusDecode
+          </span>
+          </div>
+          {username && <span className="username-display">{username}</span>}
         </div>
         <Routes>
-          <Route path='/' element={
-            <div>
-              <div className='main-bg'></div>
-              <div className='text-box'>
-                <p style={{ fontSize: '20px' }}>Decode the virus’s genetic code,<br />
-                  analyze its mutations,<br />
-                  and determine the vaccine sequence.</p>
+          <Route
+            path="/"
+            element={
+              <div>
+                <div className="main-bg"></div>
+                <div className="text-box">
+                  <p style={{ fontSize: "20px" }}>
+                    Decode the virus’s genetic code,
+                    <br />
+                    analyze its mutations,
+                    <br />
+                    and determine the vaccine sequence.
+                  </p>
+                </div>
+                <button
+                  className="image-button"
+                  onClick={() => {
+                    handleNavigate("inputSeq");
+                  }}
+                ></button>
               </div>
-              <button className="image-button" onClick={() => { handleNavigate('inputSeq') }}></button>
-            </div>} />
+            }
+          />
 
-          <Route path="/inputSeq" element={<InputSeq />} />
+          <Route
+            path="/inputSeq"
+            element={<InputSeq setUsername={setUsername} />}
+          />
           <Route path="/analysis" element={<Analysis />} />
-
         </Routes>
       </div>
-
-
-
-
     </div>
   );
 }
