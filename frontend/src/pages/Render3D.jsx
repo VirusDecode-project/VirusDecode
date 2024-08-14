04e16a1ac6cd112726d609ae2e60aea5c1c36053
kky107102 <kky107102@ajou.ac.kr>
2024-08-15 00:16:31 +0900

    feat: 렌더링 style과 pdb id 사용자가 선택하는 기능 추가

--- frontend/src/pages/Render3D.jsx
+++ frontend/src/pages/Render3D.jsx
@@ -1,29 +1,73 @@
 import Viztein from 'viztein';
+import React, { useEffect, useState } from 'react';
 import "./Render3D.css";
 
 function Render3D() {
+  const [PDBids, setPDBids] = useState(['8VCI', '8UYS', '7O7Y', '7O7Z', '7O81', '7O80']); //test
+  const [selectedPDBid, setSelectedPDBid] = useState("8VCI"); // test
+  const [representation, setRepresentation] = useState("default"); 
+
+  /*useEffect(() => {
+    const fetchData = async () => {
+      const url = 'url'; // backend url
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.status === 'success') {
+          setPDBids(data.PDBids); 
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);*/
+  
   const viewportStyle = {
     width: '900px',
     height: '900px',
   };
 
-  const pdbId = "5r7y"; // default, SARS-CoV-2
-
   const refData = {
-    filename: `https://files.rcsb.org/download/${pdbId}.pdb`,
-    config: [{
-      type: 'addRepresentation',
-      input: 'ball+stick'
-    }]
+    filename: `https://files.rcsb.org/download/${selectedPDBid}.pdb`, // 에러처리 필요
+    ...(representation !== "default" && { // style 지정
+      config: [{
+        type: 'addRepresentation',
+        input: representation 
+      }]
+    })
   };
+
+  const vizteinKey = `${selectedPDBid}-${representation}`; // PDBid 또는 style 변경 시 렌더링 업데이트에 필요한 key
 
   return (
     <div className='reference3D'>
-      <h4>Reference</h4>
-      <Viztein data={refData} viewportStyle={viewportStyle} />
-      <h5>{pdbId}.pdb</h5>
+      <div className='left-column'>
+        <h4>Reference</h4>
+        <div className='representation-container'>
+          <select
+            className="style"
+            value={representation}
+            onChange={(e) => setRepresentation(e.target.value)}
+          >
+            <option value="default"> Default </option>
+            <option value="ball+stick"> Ball + Stick </option>
+            <option value="cartoon"> Cartoon </option>
+          </select>
+        <Viztein key={vizteinKey} data={refData} viewportStyle={viewportStyle} />
+        </div>
+        <h5>{selectedPDBid}.pdb</h5>
+      </div>
+      <div className='right-column'>
+        <ul>
+          {PDBids && PDBids.map((id) => (
+            <li key={id} onClick={() => setSelectedPDBid(id)}>
+              {id}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default Render3D;
