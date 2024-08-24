ead2ff212842e055dc2bc2c972defc842e49067c
kky107102 <kky107102@ajou.ac.kr>
2024-08-25 03:58:00 +0900

    chore: 3d title추가, helpmodal추가

--- frontend/src/pages/analysis.js
+++ frontend/src/pages/analysis.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
@@ -65,34 +65,39 @@ function Analysis() {
 
   return (
     <div>
-        <div className={`analysis-container ${show ? 'shrink' : ''}`}>
-          <>
-            <Nav variant="tabs" defaultActiveKey="link0" className="justify-content-start">
-              <Nav.Item>
-                <Nav.Link eventKey="link0" active={tab === 0} onClick={() => setTab(0)}>Alignment</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="link1" active={tab === 1} onClick={() => setTab(1)}>mRNA design</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="link2" active={tab === 2} onClick={() => setTab(2)}>3D viewer</Nav.Link>
-              </Nav.Item>
-              
-            </Nav>
-            <Tab tab={tab} setTab={setTab} responseData={responseData} />
-          </>
-        </div>
+      <div className={`analysis-container ${show ? 'shrink' : ''}`}>
+        <>
+          <Nav variant="tabs" defaultActiveKey="link0" className="justify-content-start">
+            <Nav.Item>
+              <Nav.Link eventKey="link0" active={tab === 0} onClick={() => setTab(0)}>Alignment</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link1" active={tab === 1} onClick={() => setTab(1)}>mRNA design</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link2" active={tab === 2} onClick={() => setTab(2)}>3D viewer</Nav.Link>
+            </Nav.Item>
+
+          </Nav>
+          <Tab tab={tab} setTab={setTab} responseData={responseData} />
+        </>
+      </div>
     </div>
   );
 }
 
 function Tab({ tab, setTab, responseData }) {
+  const [modalRegion, setModalRegion] = useState('');
+
+  const handleModalRegion = (region) => {
+    setModalRegion(region);
+  }
   return (
     <div>
       {/* 현재 탭 상태에 따라 다른 컴포넌트 렌더링 */}
-      {tab === 0 && <Alignment responseData={responseData} setTab={setTab} />}
+      {tab === 0 && <Alignment responseData={responseData} setTab={setTab} onRegionUpdate={handleModalRegion} />}
       {tab === 1 && <MRNAdesign />}
-      {tab === 2 && <Render3D />}
+      {tab === 2 && <Render3D region={modalRegion} />}
     </div>
   );
 }
