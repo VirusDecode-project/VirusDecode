6d48501612b8675f9d5d33f6f5acd336cf708d19
GK <weeeeek2@naver.com>
2024-08-25 05:56:56 +0900

    refactor: Refactor for error handling

--- backend/src/main/java/VirusDecode/backend/controller/InputSeqController.java
+++ backend/src/main/java/VirusDecode/backend/controller/InputSeqController.java
@@ -10,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 @RestController
 @RequestMapping("/inputSeq")
 public class InputSeqController {
@@ -21,39 +19,27 @@ public class InputSeqController {
 
     // /inputSeq/reference 엔드포인트에 대한 POST 요청 처리
     @PostMapping("/reference")
-    public ResponseEntity<Map> getMetadata(@RequestBody ReferenceDTO request) {
+    public ResponseEntity<String> getMetadata(@RequestBody ReferenceDTO request) {
         String sequenceId = request.getSequenceId();  // 요청에서 시퀀스 ID 추출
-        Map<String, Object> metadata = PythonScriptExecutor.executePythonScriptAsObjectMap("bioinformatics/data/metadata.json","1", sequenceId);
-
-        // GK - 비정상적인 nucleotide 값을 처리하는 부분
-        if (metadata == null || metadata.isEmpty()) {
-            // 메타데이터가 없는 경우 상태 코드 204 No Content 반환
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-        }
-
-        // 메타데이터가 있는 경우 상태 코드 200 OK와 함께 메타데이터 반환
-        return ResponseEntity.ok(metadata);
+        return PythonScriptExecutor.executePythonScript("bioinformatics/data/metadata.json","1", sequenceId);
     }
 
     // /inputSeq/alignment 엔드포인트에 대한 POST 요청 처리
     @PostMapping("/alignment")
-    public ResponseEntity<Map<String, Object>> getAlignment(@RequestBody(required = false) VarientDTO request) {
+    public ResponseEntity<String> getAlignment(@RequestBody(required = false) VarientDTO request) {
         try {
             // 서비스 호출을 통해 사용자 입력을 파일로 저장
             String savedFilePath = fastaFileService.saveFastaContent(request);
 
-            // 저장된 파일을 이용해 alignment 처리 후 결과 반환
-            Map<String, Object> alignmentResult = PythonScriptExecutor.executePythonScriptAsObjectMap("bioinformatics/data/alignment_data.json", "2");
-
             // 결과를 상태 코드 200 OK와 함께 반환
-            return ResponseEntity.ok(alignmentResult);
+            return PythonScriptExecutor.executePythonScript("bioinformatics/data/alignment_data.json", "2");
         } catch (IOException e) {
             // 파일 저장 중 오류가 발생한 경우 상태 코드 500과 함께 오류 메시지 반환
-            Map<String, Object> errorResponse = new HashMap<>();
-            errorResponse.put("status", "error");
-            errorResponse.put("message", "파일 저장 중 오류 발생: " + e.getMessage());
+//            Map<String, Object> errorResponse = new HashMap<>();
+//            errorResponse.put("status", "error");
+//            errorResponse.put("message", "파일 저장 중 오류 발생: " + e.getMessage());
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일 저장 중 오류 발생");
         }
     }
 }
