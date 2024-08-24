033905bd4d4123a51c4b80b6505851fc53cf6d63
GK <weeeeek2@naver.com>
2024-08-25 02:59:04 +0900

    feat: Add loading components

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,30 @@
+// Loading.js
+import React, { useState, useEffect } from 'react';
+import loadingImage from '../image/loading.png'; // Adjust the path as necessary
+import './Loading.css'; // Optional: You can add styles for the loading component
+
+const Loading = ({text}) => {
+  const [loadingText, setLoadingText] = useState(text);
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setLoadingText((prev) => {
+        if (prev.endsWith('...')) return text;
+        return prev + '.';
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className="loading-container">
+      <img src={loadingImage} alt="Loading..." className="loading-image" />
+      <div className="loading-text">{loadingText}</div>
+    </div>
+  );
+};
+
+export default Loading;
