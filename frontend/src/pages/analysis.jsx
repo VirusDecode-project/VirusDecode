0d3c84d9816f133d0eee1ef33de54ddd8b8cb947
kky107102 <kky107102@ajou.ac.kr>
2024-08-25 22:14:34 +0900

    chore: tab 비활성화, 3d리스트 hover시 text width수정

--- frontend/src/pages/analysis.jsx
+++ frontend/src/pages/analysis.jsx
@@ -6,6 +6,8 @@ import Tab from '../components/Tab';
 
 function Analysis() {
   let [tab, setTab] = useState(0);
+  const [mRNAReceived, setMRNAReceived] = useState(false);
+  const [PDBReceived, setPDBReceived] = useState(false);
   const [show, setShow] = useState(false);
   const location = useLocation();
   const responseData = location.state?.responseData || null;
@@ -19,13 +21,19 @@ function Analysis() {
               <Nav.Link eventKey="link0" active={tab === 0} onClick={() => setTab(0)}>Alignment</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link1" active={tab === 1} onClick={() => setTab(1)}>mRNA design</Nav.Link>
+              <Nav.Link eventKey="link1" active={tab === 1} onClick={() => setTab(1)} disabled={!mRNAReceived}>mRNA design</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link2" active={tab === 2} onClick={() => setTab(2)}>3D viewer</Nav.Link>
+              <Nav.Link eventKey="link2" active={tab === 2} onClick={() => setTab(2)} disabled={!PDBReceived}>3D viewer</Nav.Link>
             </Nav.Item>
           </Nav>
-          <Tab tab={tab} setTab={setTab} responseData={responseData} />
+          <Tab
+            tab={tab}
+            setTab={setTab}
+            responseData={responseData}
+            setMRNAReceived={setMRNAReceived}
+            setPDBReceived={setPDBReceived}
+          />
         </>
       </div>
     </div>
