3ae85bff876f5915957d2a1f353ab3eaa85a3cc5
GK <weeeeek2@naver.com>
2024-08-26 18:30:07 +0900

    refactor: Resolve npm start warning

--- frontend/src/components/HelpModal.js
+++ frontend/src/components/HelpModal.js
@@ -34,15 +34,15 @@ function HelpModal({ isOpen, onClose }) {
                 <p className='helpTitle'>mRNA Conversion Instructions</p>
                 <p className='helpLevel'>1. Select CDS</p>
                 <p className='helpContents'>Click on the CDS you wish to convert for mRNA design.</p>
-                <img className='helpImageWide' src={helpImg1} />
+                <img className='helpImageWide' src={helpImg1} alt="Step 1: Select CDS"/>
                 <p className='helpLevel'>2. Choose sublineage and input amino acid range</p>
                 <p className='helpContents'>- Click on the appropriate sublineage from the initial input sequence.</p>
                 <p className='helpContents'>- Enter the start and end positions for the amino acids.</p>
                 <p className='helpWarnning'>*Note: Do not exceed 500 amino acids.</p>
-                <img className='helpImage' src={helpImg2} />
+                <img className='helpImage' src={helpImg2} alt="Step 2: Choose sublineage and input amino acid range" />
                 <p className='helpLevel'>3. Convert</p>
                 <p className='helpContents'>Click the "Convert" button to proceed with the mRNA design.</p>
-                <img className='helpImage' src={helpImg3} />
+                <img className='helpImage' src={helpImg3} alt="Step 3: Convert" />
             </div>
             }
         </div>
