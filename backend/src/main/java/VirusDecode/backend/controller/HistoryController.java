cae9e0082811fe50426adb58b0555e0b030d2544
GK <weeeeek2@naver.com>
2024-09-08 06:27:42 +0900

    chore: Add history and data path, Update REST API

--- backend/src/main/java/VirusDecode/backend/controller/HistoryController.java
+++ backend/src/main/java/VirusDecode/backend/controller/HistoryController.java
@@ -25,36 +25,39 @@ public class HistoryController {
     public HistoryController(JsonFileService jsonFileService) {
         this.jsonFileService = jsonFileService;
     }
+
     private static final Path currentDir = Paths.get("").toAbsolutePath();
+    private static final Path HISTORY_DIR = currentDir.resolve("history");
+    private static final Path DATA_DIR = currentDir.resolve("data");
 
     @PostMapping("/create")
     public ResponseEntity<String> createHistory(@RequestBody HistoryDTO request) {
         String historyName = request.getHistoryName();
         try {
-            createHistory(historyName);
+                performCreateHistory(historyName);
             return ResponseEntity.ok("History created successfully.");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to create history: " + e.getMessage());
         }
     }
 
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public ResponseEntity<String> deleteHistory(@RequestBody HistoryDTO request) {
         String historyName = request.getHistoryName();
         try {
-            deleteHistory(historyName);
+            performDeleteHistory(historyName);
             return ResponseEntity.ok("History deleted successfully.");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to delete history: " + e.getMessage());
         }
     }
 
-    @PostMapping("/rename")
+    @PutMapping("/rename")
     public ResponseEntity<String> renameHistory(@RequestBody HistoryDTO request) {
         String historyName = request.getHistoryName();
         String newName = request.getNewName();
         try {
-            renameHistory(historyName, newName);
+            performRenameHistory(historyName, newName);
             return ResponseEntity.ok("History renamed successfully.");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to rename history: " + e.getMessage());
@@ -65,7 +68,7 @@ public class HistoryController {
     public ResponseEntity<String> getHistory(@RequestBody HistoryDTO request) {
         String historyName = request.getHistoryName();
         try {
-            getHistory(historyName);
+            performGetHistory(historyName);
             return jsonFileService.readJsonFile("alignment_data.json");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to get history: " + e.getMessage());
@@ -75,7 +78,7 @@ public class HistoryController {
     @GetMapping("/list")
     public ResponseEntity<String> listHistory() {
         try {
-            String jsonHistoryList = getHistoryList();
+            String jsonHistoryList = performHistoryList();
             return ResponseEntity.ok(jsonHistoryList);
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to list history: " + e.getMessage());
@@ -85,7 +88,7 @@ public class HistoryController {
     @GetMapping("/deleteData")
     public ResponseEntity<String> deleteData() {
         try {
-            getDeleteData();
+            performDeleteData();
             return ResponseEntity.ok("Data delete successfully.");
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to delete data: " + e.getMessage());
@@ -93,14 +96,12 @@ public class HistoryController {
     }
 
 
-    private void createHistory(String historyName) throws IOException {
-        Path newDir = currentDir.resolve("history").resolve(historyName);
+    private void performCreateHistory(String historyName) throws IOException {
+        Path newDir = HISTORY_DIR.resolve(historyName);
         Files.createDirectories(newDir);
 
-        Path dataDir = currentDir.resolve("data");
-
-        if (Files.exists(dataDir)) {
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataDir)) {
+        if (Files.exists(DATA_DIR)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(DATA_DIR)) {
                 for (Path filePath : stream) {
                     if (Files.isRegularFile(filePath)) {
                         Files.copy(filePath, newDir.resolve(filePath.getFileName()), StandardCopyOption.REPLACE_EXISTING);
@@ -110,8 +111,8 @@ public class HistoryController {
         }
     }
 
-    private void deleteHistory(String historyName) throws IOException {
-        Path newDir = currentDir.resolve("history").resolve(historyName);
+    private void performDeleteHistory(String historyName) throws IOException {
+        Path newDir = HISTORY_DIR.resolve(historyName);
 
         if (Files.exists(newDir)) {
             Files.walk(newDir)
@@ -121,22 +122,21 @@ public class HistoryController {
         }
     }
 
-    private void renameHistory(String historyName, String newHistoryName) throws IOException {
-        Path oldDir = currentDir.resolve("history").resolve(historyName);
-        Path newDir = currentDir.resolve("history").resolve(newHistoryName);
+    private void performRenameHistory(String historyName, String newHistoryName) throws IOException {
+        Path oldDir = HISTORY_DIR.resolve(historyName);
+        Path newDir = HISTORY_DIR.resolve(newHistoryName);
 
         if (Files.exists(oldDir)) {
             Files.move(oldDir, newDir, StandardCopyOption.REPLACE_EXISTING);
         }
     }
 
-    private void getHistory(String historyName) throws IOException {
-        Path sourceDir = currentDir.resolve("history").resolve(historyName);
-        Path dataDir = currentDir.resolve("data");
+    private void performGetHistory(String historyName) throws IOException {
+        Path sourceDir = HISTORY_DIR.resolve(historyName);
 
         if (Files.exists(sourceDir)) {
-            if (Files.exists(dataDir)) {
-                try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataDir)) {
+            if (Files.exists(DATA_DIR)) {
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(DATA_DIR)) {
                     for (Path filePath : stream) {
                         if (Files.isRegularFile(filePath)) {
                             Files.delete(filePath);
@@ -144,26 +144,24 @@ public class HistoryController {
                     }
                 }
             }
-            Files.createDirectories(dataDir);
+            Files.createDirectories(DATA_DIR);
 
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourceDir)) {
                 for (Path filePath : stream) {
                     if (Files.isRegularFile(filePath)) {
-                        Files.copy(filePath, dataDir.resolve(filePath.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                        Files.copy(filePath, DATA_DIR.resolve(filePath.getFileName()), StandardCopyOption.REPLACE_EXISTING);
                     }
                 }
             }
         }
     }
 
-    private String getHistoryList() throws IOException {
-        Path historyDir = currentDir.resolve("history");
-
-        if (Files.notExists(historyDir)) {
-            Files.createDirectories(historyDir);
+    private String performHistoryList() throws IOException {
+        if (Files.notExists(HISTORY_DIR)) {
+            Files.createDirectories(HISTORY_DIR);
         }
 
-        List<String> historyList = Arrays.stream(new File(historyDir.toString()).listFiles())
+        List<String> historyList = Arrays.stream(new File(HISTORY_DIR.toString()).listFiles())
                 .sorted(Comparator.comparingLong(File::lastModified))  // 마지막 수정 시간을 기준으로 정렬
                 .map(File::getName)  // 파일 이름만 가져오기
                 .collect(Collectors.toList());
@@ -173,12 +171,9 @@ public class HistoryController {
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(historyList);
     }
 
-    private void getDeleteData() throws IOException {
-        Path newDir = currentDir.resolve("data");
-
-
-        if (Files.exists(newDir)) {
-            Files.walk(newDir)
+    private void performDeleteData() throws IOException {
+        if (Files.exists(DATA_DIR)) {
+            Files.walk(DATA_DIR)
                     .sorted(Comparator.reverseOrder())
                     .map(Path::toFile)
                     .forEach(File::delete);
