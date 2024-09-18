9417d9c9090b1fb0de50f0171635e0ab88f7349c
kky107102 <kk107102@ajou.ac.kr>
2024-09-19 02:16:09 +0900

    fix: Move to types

--- frontend/src/fornac.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'fornac' {
-  export class FornaContainer {
-      constructor(element: HTMLElement, options?: any);
-      addRNA(structure: string, options?: any): void;
-      clearNodes(): void;
-  }
-}
