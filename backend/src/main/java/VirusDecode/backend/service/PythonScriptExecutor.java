f024fffde6561ad82053a60addd0892c7bbf189e
Guho-Kim <weeeeek2@naver.com>
2024-09-22 03:37:21 +0900

    fix: Modify python script error message and do logger when error occur

--- backend/src/main/java/VirusDecode/backend/service/PythonScriptExecutor.java
+++ backend/src/main/java/VirusDecode/backend/service/PythonScriptExecutor.java
@@ -57,13 +57,6 @@ public class PythonScriptExecutor {
                 while ((line = stdError.readLine()) != null) {
                     errorOutput.append(line).append("\n");
                 }
-
-                if (!output.isEmpty()) {
-                    logger.info("Python script output: \n{}", output);
-                }
-                if (!errorOutput.isEmpty()) {
-                    logger.error("Python script error output: \n{}", errorOutput);
-                }
             }
 
             int exitCode = process.waitFor();
@@ -71,11 +64,17 @@ public class PythonScriptExecutor {
             // 파이썬 오류 코드 처리
             if (exitCode != 0) {
                 logger.error("Python script 종료 코드: {}", exitCode);
+                if (!output.isEmpty()) {
+                    logger.info("Python script output: \n{}", output);
+                }
+                if (!errorOutput.isEmpty()) {
+                    logger.error("Python script error output: \n{}", errorOutput);
+                }
                 return switch (exitCode) {
                     case 11 -> ResponseEntity.status(500).body("NCBI에 요청한 nucleotide ID가 존재하지 않습니다.");
                     case 21 -> ResponseEntity.status(500).body("MUSCLE 다중 서열 정리에 문제가 발생하였습니다.");
                     case 31 -> ResponseEntity.status(500).body("선택된 구간에 유효한 서열이 없습니다.");
-                    case 32 -> ResponseEntity.status(500).body("LinearDesign 실행파일이 정상적으로 만들어지지 않았습니다.");
+                    case 32 -> ResponseEntity.status(500).body("LinearDesign 실행파일이 정상적으로 만들어지지 않았습니다. Linux 또는 Max 사용자가 맞으신가요?");
                     case 33 -> ResponseEntity.status(500).body("LinearDesign 디렉토리가 원하는 위치가 존재하지 않습니다.");
                     case 41 -> ResponseEntity.status(500).body("RCSB PDB 서버로부터 PDB ID 검색에 실패하였습니다.");
                     case 42 -> ResponseEntity.status(500).body("RCSB PDB 서버의 문제로 3D viewer 데이터 로드에 실패하였습니다.");
