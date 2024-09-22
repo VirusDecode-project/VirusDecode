48f50224c66870007275b2b7f2c953f443c92419
GK <weeeeek2@naver.com>
2024-09-22 16:48:44 +0900

    fix: Modify using json service method from JsonFileService to JsonDataService

--- backend/src/main/java/VirusDecode/backend/controller/AnalysisController.java
+++ backend/src/main/java/VirusDecode/backend/controller/AnalysisController.java
@@ -2,8 +2,8 @@ package VirusDecode.backend.controller;
 
 import VirusDecode.backend.dto.LinearDesignDTO;
 import VirusDecode.backend.dto.PdbDTO;
+import VirusDecode.backend.service.JsonDataService;
 import VirusDecode.backend.service.PythonScriptExecutor;
-import VirusDecode.backend.service.JsonFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +13,12 @@ import java.util.Map;
 @RequestMapping("/analysis")
 public class AnalysisController {
     private final PythonScriptExecutor pythonScriptExecutor;
-    private final JsonFileService jsonFileService;
+    private JsonDataService jsonDataService;
 
     @Autowired
-    public AnalysisController(PythonScriptExecutor pythonScriptExecutor, JsonFileService jsonFileService) {
+    public AnalysisController(PythonScriptExecutor pythonScriptExecutor, JsonDataService jsonDataService) {
         this.pythonScriptExecutor = pythonScriptExecutor;
-        this.jsonFileService = jsonFileService;
+        this.jsonDataService = jsonDataService;
     }
 
 
@@ -31,31 +31,52 @@ public class AnalysisController {
         String start = String.valueOf(request.getStart());
         String end = String.valueOf(request.getEnd());
 
-        return pythonScriptExecutor.executePythonScript("3", region, varientName, start, end);
+        String referenceId = jsonDataService.parseSequenceIdFromMetadata(jsonDataService.getJsonData("metadata"));
+        String alignmentIndex = jsonDataService.jsonParser(jsonDataService.getJsonData("alignment"), "alignment_index");
+        String alignedSequences = jsonDataService.jsonParser(jsonDataService.getJsonData("alignment"), "aligned_sequences");
+        System.out.println(referenceId);
+        System.out.println(alignmentIndex);
+        System.out.println(alignedSequences);
+
+        ResponseEntity<String> scriptResponse = pythonScriptExecutor.executePythonScript("3", referenceId, alignmentIndex, alignedSequences, region, varientName, start, end);
+        if (scriptResponse.getStatusCode().is2xxSuccessful()) {
+            jsonDataService.saveJsonData("linearDesign", scriptResponse.getBody());
+        }
+        return scriptResponse;
     }
 
     // /analysis/pdb 엔드포인트에 대한 POST 요청 처리
     @PostMapping("/pdb")
     public ResponseEntity<String> getPdb(@RequestBody PdbDTO request) {
         String gene = request.getGene();
-        return pythonScriptExecutor.executePythonScript("4", gene);
+        String referenceId = jsonDataService.parseSequenceIdFromMetadata(jsonDataService.getJsonData("metadata"));
+        String alignmentIndex = jsonDataService.jsonParser(jsonDataService.getJsonData("alignment"), "alignment_index");
+        String alignedSequences = jsonDataService.jsonParser(jsonDataService.getJsonData("alignment"), "aligned_sequences");
+        System.out.println(referenceId);
+        System.out.println(alignmentIndex);
+        System.out.println(alignedSequences);
+        ResponseEntity<String> scriptResponse = pythonScriptExecutor.executePythonScript("4", referenceId, alignmentIndex, alignedSequences, gene);
+        if (scriptResponse.getStatusCode().is2xxSuccessful()) {
+            jsonDataService.saveJsonData("pdb", scriptResponse.getBody());
+        }
+        return scriptResponse;
     }
 
     // /analysis/re-alignment 엔드포인트에 대한 POST 요청 처리
     @GetMapping("/re-alignment")
     public ResponseEntity<String> sendJsonFile() {
-        return jsonFileService.readJsonFile("alignment.json");
+        return ResponseEntity.ok(jsonDataService.getJsonData("alignment"));
     }
 
     // /analysis/re-mrnadesign 엔드포인트에 대한 POST 요청 처리
     @GetMapping("/re-mrnadesign")
     public ResponseEntity<String> re_mrna_design_json() {
-        return jsonFileService.readJsonFile("linearDesign.json");
+        return ResponseEntity.ok(jsonDataService.getJsonData("linearDesign"));
     }
 
     // /analysis/render3d 엔드포인트에 대한 GET 요청 처리
     @GetMapping("/re-render3d")
     public ResponseEntity<String> return_pdb_list() {
-        return jsonFileService.readJsonFile("pdb.json");
+        return ResponseEntity.ok(jsonDataService.getJsonData("pdb"));
     }
 }
