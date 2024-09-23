34a62bc91c50ec724794ea0a02cebdbc5a9bd2ae
GK <weeeeek2@naver.com>
2024-09-24 01:02:29 +0900

    fix: Update for Jpa repository

--- backend/src/main/java/VirusDecode/backend/repository/JsonDataRepository.java
+++ backend/src/main/java/VirusDecode/backend/repository/JsonDataRepository.java
@@ -7,15 +7,8 @@ import java.util.Optional;
 public interface JsonDataRepository {
 
     JsonDataEntity save(JsonDataEntity jsonDataEntity); // Save or update JSON data
-
-//    Optional<JsonDataEntity> findById(Long id); // Find JSON data by ID
     Optional<JsonDataEntity> findByName(String name); // Find JSON data by Name
-
     Iterable<JsonDataEntity> findAll(); // Get all stored JSON data
-
-//    void deleteById(Long id); // Delete JSON data by ID
-
     void deleteByName(String name); // Delete JSON data by Name
-
     void deleteAll(); // Delete all JSON data (명시적으로 추가)
 }
