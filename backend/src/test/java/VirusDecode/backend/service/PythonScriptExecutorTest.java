b1cfc8a5f742799f30c741314340943f3cd83125
GK <weeeeek2@naver.com>
2024-09-23 16:04:12 +0900

    fix:PythonScriptExecutorTest coverage 88%

--- backend/src/test/java/VirusDecode/backend/service/PythonScriptExecutorTest.java
+++ backend/src/test/java/VirusDecode/backend/service/PythonScriptExecutorTest.java
@@ -2,55 +2,41 @@ package VirusDecode.backend.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.http.ResponseEntity;
-
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class PythonScriptExecutorTest {
-
     private ProcessBuilder processBuilderMock;
     private Process processMock;
     private PythonScriptExecutor pythonScriptExecutor;
-
+    private InputStream inputStreamMock;
+    private InputStream errorStreamMock;
+    private PythonScriptExecutor pythonScriptExecutorMock;
     @BeforeEach
     void setUp() {
-        // Mocking the ProcessBuilder, Process, and BufferedReader
         processBuilderMock = mock(ProcessBuilder.class);
         processMock = mock(Process.class);
         pythonScriptExecutor = new PythonScriptExecutor();
+        pythonScriptExecutorMock = Mockito.spy(new PythonScriptExecutor());
+        inputStreamMock = new ByteArrayInputStream("".getBytes());
+        errorStreamMock = new ByteArrayInputStream("".getBytes());
     }
 
     @Test
     void testExecutePythonScriptSuccess() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(0);
-
         // Act
         ResponseEntity<String> response = pythonScriptExecutor.executePythonScript("1", "NC_045512");
 
         // Assert
         assertEquals(200, response.getStatusCodeValue());
     }
+
     @Test
     void testExecutePythonScriptSuccess2() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(0);
-
         String fastaContent = "";
         String metadataJson = "{\n" +
                 "    \"Sequence ID\": \"NC_001803.1\",\n" +
@@ -68,54 +54,17 @@ class PythonScriptExecutorTest {
 
     @Test
     void testNotAvailableNucleotide() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(11);
-
         // Act
         ResponseEntity<String> response = pythonScriptExecutor.executePythonScript("1", "noExist");
 
-        // Assert: Verify the error message for exit code 11
-        assertEquals(500, response.getStatusCodeValue());
-        assertEquals("NCBI에 요청한 nucleotide ID가 존재하지 않습니다.", response.getBody());
-    }
-
-    @Test
-    void testForCase1() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(1);
-
-        // Act
-        ResponseEntity<String> response = pythonScriptExecutor.executePythonScript("1", anyString());
-
         // Assert: Verify the error message for exit code 1
         assertEquals(500, response.getStatusCodeValue());
-        assertEquals("필요한 파이썬 환경이 제대로 설치되지 않았습니다.\nVirusDecode Github를 참고하세요.", response.getBody());
+        assertEquals("NCBI에 요청한 nucleotide ID가 존재하지 않습니다.", response.getBody());
     }
 
 
     @Test
     void testEmptyArgument() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(2);
-
         // Act
         ResponseEntity<String> response = pythonScriptExecutor.executePythonScript();
 
@@ -126,14 +75,6 @@ class PythonScriptExecutorTest {
 
     @Test
     void testLinearDesignCase31() throws Exception {
-        // Arrange: Create a mock InputStream for process output
-        InputStream inputStreamMock = mock(InputStream.class);
-        InputStream errorStreamMock = mock(InputStream.class);
-
-        when(processBuilderMock.start()).thenReturn(processMock);
-        when(processMock.getInputStream()).thenReturn(inputStreamMock);
-        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
-        when(processMock.waitFor()).thenReturn(2);
         String metadataJson = "{\n" +
                 "    \"Sequence ID\": \"NC_001803.1\",\n" +
                 "    \"Name\": \"NC_001803\",\n" +
@@ -149,5 +90,118 @@ class PythonScriptExecutorTest {
         // Assert: Verify the error message for exit code 11
         assertEquals(500, response.getStatusCodeValue());
         assertEquals("선택된 구간에 유효한 서열이 없습니다.", response.getBody());
+    }
+
+    @Test
+    void testExitCode1() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(1);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("필요한 파이썬 환경이 제대로 설치되지 않았습니다.\nVirusDecode Github를 참고하세요.", response.getBody());
+    }
+    @Test
+    void testExitCode21() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(21);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("MUSCLE 다중 서열 정리에 문제가 발생하였습니다.", response.getBody());
+    }
+    @Test
+    void testExitCode32() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(32);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("LinearDesign 실행파일이 정상적으로 만들어지지 않았습니다.\nLinux 또는 Max 사용자가 맞으신가요?", response.getBody());
+    }
+    @Test
+    void testExitCode33() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(33);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("LinearDesign 디렉토리가 원하는 위치가 존재하지 않습니다.", response.getBody());
+    }
+    @Test
+    void testExitCode41() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(41);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("RCSB PDB 서버로부터 PDB ID 검색에 실패하였습니다.", response.getBody());
+    }
+    @Test
+    void testExitCode42() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(42);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("RCSB PDB 서버의 문제로 3D viewer 데이터 로드에 실패하였습니다.", response.getBody());
+    }
+    @Test
+    void testExitCodeDefault() throws Exception {
+        // Mock 설정
+        doReturn(processBuilderMock).when(pythonScriptExecutorMock).createProcessBuilder(anyList());
+        when(processBuilderMock.start()).thenReturn(processMock);
+        when(processMock.getInputStream()).thenReturn(inputStreamMock);
+        when(processMock.getErrorStream()).thenReturn(errorStreamMock);
+        when(processMock.waitFor()).thenReturn(5);
+
+        // Act: Python 스크립트 실행
+        ResponseEntity<String> response = pythonScriptExecutorMock.executePythonScript();
+
+        // Assert: 결과 검증
+        assertEquals(500, response.getStatusCodeValue());
+        assertEquals("Error executing Python script: ", response.getBody());
     }
 }
