70cdf711139bad70bb56257addf064077312d531
GK <weeeeek2@naver.com>
2024-09-25 10:44:10 +0900

    feat: Create User database logic

---
+++ backend/src/main/java/VirusDecode/backend/entity/User.java
@@ -0,0 +1,58 @@
+package VirusDecode.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<JsonData> jsonDataList = new ArrayList<>(); // JsonData 리스트
+
+
+
+
+    public List<JsonData> getJsonDataList() {
+        return jsonDataList;
+    }
+
+    public void setJsonDataList(List<JsonData> jsonDataList) {
+        this.jsonDataList = jsonDataList;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
