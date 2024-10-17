42bedb81b5573e57341b78982713ca8e1b1b13f8
kky107102 <kky107102@ajou.ac.kr>
2024-10-18 01:27:00 +0900

    test: Update Headerbar

--- frontend/cypress/e2e/Headerbar.cy.js
+++ frontend/cypress/e2e/Headerbar.cy.js
@@ -1,63 +1,44 @@
-// 로그아웃: 로그아웃 및 메인페이지 이동 #3
-// 로고 및 사이트명: 메인페이지로 이동 #1
-// 회원 아이콘: 회원 프로필 정보 확인 #2
-// 히스토리 아이콘: 히스토리 여닫기 #4
-// 편집 아이콘: 서열 입력 페이지로 이동 #5
-describe('HeaderBar Test', () => {
-  const guestlogin = () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('.decode-button').click();
-    cy.get('.stayLoggedOutBtn').click(); 
-  }
-  beforeEach(() => {
-    guestlogin();
-    cy.visit('http://localhost:3000/inputSeq');
-    cy.intercept('POST', '/api/auth/userinfo').as('userinfoRequest');
-  });
-
-  it('navigates to home when logo is clicked', () => {
-    // #1 home page로 이동
+describe('1. 로고 및 사이트명', () => {
+  it('1-1. 메인페이지로 이동', () => {
+    cy.signupAndLoginIfDuplicate('testFName','testLName','testId','testPw','testPw');
     cy.get('.logo-text').should('be.visible').click();
     cy.url().should('include', '/');
   });
+});
 
-  it('toggles user info menu when user icon is clicked (logged in)', () => {
-    // #2 로그인 상태에서 userInfo 아이콘 클릭 시 메뉴 열림
-    cy.get('.user-icon').click({ multiple: true });
+describe('2. 회원 아이콘', () => {
+  it('2-1. 회원 프로필 정보 확인', () => {
+    cy.signupAndLoginIfDuplicate('testFName','testLName','testId','testPw','testPw');
+    cy.intercept('POST', '/api/auth/userinfo').as('userinfoRequest');
+    cy.get('.user-icon').click();
     cy.wait('@userinfoRequest').then((interception) => {
       expect(interception.response.statusCode).to.eq(200);
-      cy.get('.userInfo-menu')
+      cy.get('.user-icon').click();
+      cy.get('.userInfo-menu').invoke('show')
       .should('be.visible')
-      .and('contain', 'Guest');
+      .and('contain', 'testFName')
+      .and('contain', 'testId');
       
       // 다시 클릭하여 닫힘
       cy.get('.user-icon').click();
       cy.get('.userInfo-menu').should('not.exist');
     });
   });
+});
 
-  it('logs out and redirects to home after clicking logout button (logged in)', () => {
-    // #3 로그아웃 버튼 클릭 시 로그아웃 처리 및 홈으로 이동
-    cy.get('.user-icon').click({ multiple: true });
-    cy.wait('@userinfoRequest').then((interception) => {
-      expect(interception.response.statusCode).to.eq(200);
-      cy.get('.userInfo-menu').should('be.visible');
-      cy.get('.logoutBtn').click();
-      cy.get('.userInfo-menu').should('not.exist');
-      cy.url().should('include', '/');
-    });
-  });
-
-  it('open and close sidebar when history icon is clicked', () => {
-    // #4 사이드바 여닫기
+describe('3. 히스토리 아이콘', () => {
+  it('3-1. 히스토리 여닫기', () => {
+    cy.signupAndLoginIfDuplicate('testFName','testLName','testId','testPw','testPw');
     cy.get('.sidebar .history-icon').click();
     cy.get('.sidebar').should('not.have.class', 'show');
     cy.get('.header-bar .history-icon').click();
     cy.get('.sidebar').should('have.class', 'show');
   });
+});
 
-  it('open history-modal when edit icon is clicked', () => {
-    // #5 restart
+describe('4. 편집 아이콘', () => {
+  it('4-1. 서열 입력 페이지로 이동', () => {
+    cy.signupAndLoginIfDuplicate('testFName','testLName','testId','testPw','testPw');
     cy.visit('http://localhost:3000/analysis');
     // 사이드바가 닫혀 있을 때 헤더바의 edit-icon 클릭
     cy.get('.sidebar .history-icon').click();
@@ -78,3 +59,15 @@ describe('HeaderBar Test', () => {
     cy.url().should('include', '/InputSeq');
   });
 });
+
+// it('logs out and redirects to home after clicking logout button (logged in)', () => {
+//   // #3 로그아웃 버튼 클릭 시 로그아웃 처리 및 홈으로 이동
+//   cy.get('.user-icon').click({ multiple: true });
+//   cy.wait('@userinfoRequest').then((interception) => {
+//     expect(interception.response.statusCode).to.eq(200);
+//     cy.get('.userInfo-menu').should('be.visible');
+//     cy.get('.logoutBtn').click();
+//     cy.get('.userInfo-menu').should('not.exist');
+//     cy.url().should('include', '/');
+//   });
+// });
