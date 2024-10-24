0701b2c64512ee604f4f877448df3f7f42029d44
오성현 <henryosh@oseonghyeon-ui-MacBookPro.local>
2024-10-24 20:29:28 +0900

    hotfix: testcode skeleton, 1273 -> 1276

--- frontend/cypress/e2e/Seq_Convert_modal.cy.js
+++ frontend/cypress/e2e/Seq_Convert_modal.cy.js
@@ -1,88 +1,90 @@
-describe('1. 아미노산 구간 설정 및 검증', () => {
+describe("1. 아미노산 구간 설정 및 검증", () => {
+  beforeEach(() => {
+    cy.signupAndLoginIfDuplicate(
+      "testFName",
+      "testLName",
+      "testId",
+      "testPw",
+      "testPw"
+    );
+    cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
+    cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
 
-    beforeEach(() => {
-        cy.signupAndLoginIfDuplicate(
-          "testFName",
-          "testLName",
-          "testId",
-          "testPw",
-          "testPw"
+    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
+    cy.fixture("referenceId").then((referenceId) => {
+      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
+      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
+      cy.get("button.done-button").click();
+      cy.wait("@metadataRequest").then((interception) => {
+        expect(interception.response.statusCode).to.eq(200);
+
+        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+        cy.contains("Sequence ID").should("be.visible");
+        cy.contains("Name").should("be.visible");
+        cy.contains("Description").should("be.visible");
+        cy.contains("Length").should("be.visible");
+      });
+      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
+        .parent() // 부모 요소로 이동
+        .find("textarea") // 부모 요소 아래의 textarea를 찾음
+        .type(
+          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
         );
-        cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
-        cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+      cy.get("button.next-button").click();
+    });
+  });
 
-        // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-        cy.fixture("referenceId").then((referenceId) => {
-          // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-          cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-          cy.get("button.done-button").click();
-          cy.wait("@metadataRequest").then((interception) => {
-            expect(interception.response.statusCode).to.eq(200);
+  it("1-1. 아미노산 구간 설정", () => {
+    let startValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1;
+    let endValue = Math.min(startValue + Math.floor(Math.random() * 100), 1276);
 
-            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-            cy.contains("Sequence ID").should("be.visible");
-            cy.contains("Name").should("be.visible");
-            cy.contains("Description").should("be.visible");
-            cy.contains("Length").should("be.visible");
-          });
-          cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-            .parent() // 부모 요소로 이동
-            .find("textarea") // 부모 요소 아래의 textarea를 찾음
-            .type(
-              "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-            );
-          cy.get("button.next-button").click();
-        });
-      });
+    cy.intercept("POST", "/api/analysis/linearDesign").as(
+      "linearDesignRequest"
+    );
 
-    it('1-1. 아미노산 구간 설정', () => {
-        let startValue = Math.floor(Math.random() * (1273 - 1 + 1)) + 1;
-        let endValue = Math.min(startValue + Math.floor(Math.random() * 100), 1273);
+    cy.get(".sequence-boxes").eq(0).click();
 
-        cy.intercept('POST', '/api/analysis/linearDesign').as('linearDesignRequest');
+    cy.contains(".modal-content label", "Select Coding Sequence:")
+      .find("select")
+      .select("S");
 
-        cy.get('.sequence-boxes').eq(0).click();
+    cy.contains(".modal-content label", "Start Amino Acid Position:")
+      .find('input[type="number"]')
+      .type(startValue.toString());
 
-        cy.contains('.modal-content label', 'Select Coding Sequence:')
-          .find('select')
-          .select('S');
+    cy.contains(".modal-content label", "End Amino Acid Position:")
+      .find('input[type="number"]')
+      .type(endValue.toString());
 
-        cy.contains('.modal-content label', 'Start Amino Acid Position:')
-          .find('input[type="number"]')
-          .type(startValue.toString());
+    cy.get(".modal-next-button").click();
 
-        cy.contains('.modal-content label', 'End Amino Acid Position:')
-          .find('input[type="number"]')
-          .type(endValue.toString());
+    cy.wait("@linearDesignRequest", { timeout: 50000 })
+      .its("response.statusCode")
+      .should("eq", 200);
+  });
 
-        cy.get('.modal-next-button').click();
+  it("1-2. 범위 유효성 검증", () => {
+    let startValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1277;
+    let endValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1277;
 
-        cy.wait('@linearDesignRequest', { timeout: 50000 }).its('response.statusCode').should('eq', 200);
-      });
+    cy.get(".sequence-boxes").eq(0).click();
 
-    it('1-2. 범위 유효성 검증', () => {
-        let startValue = Math.floor(Math.random() * (1273 - 1 + 1)) + 1274;
-        let endValue = Math.floor(Math.random() * (1273 - 1 + 1)) + 1274;
+    cy.contains(".modal-content label", "Select Coding Sequence:")
+      .find("select")
+      .select("S");
 
-        cy.get('.sequence-boxes').eq(0).click();
+    cy.contains(".modal-content label", "Start Amino Acid Position:")
+      .find('input[type="number"]')
+      .type(startValue.toString());
 
-        cy.contains('.modal-content label', 'Select Coding Sequence:')
-          .find('select')
-          .select('S');
+    cy.contains(".modal-content label", "End Amino Acid Position:")
+      .find('input[type="number"]')
+      .type(endValue.toString());
 
-        cy.contains('.modal-content label', 'Start Amino Acid Position:')
-          .find('input[type="number"]')
-          .type(startValue.toString());
+    cy.get(".modal-next-button").click();
 
-        cy.contains('.modal-content label', 'End Amino Acid Position:')
-          .find('input[type="number"]')
-          .type(endValue.toString());
-
-        cy.get('.modal-next-button').click();
-
-        cy.contains('index must be between').should('be.visible');
-      });
-
+    cy.contains("index must be between").should("be.visible");
+  });
 });
 
 describe("2. mRNA 서열 변환", () => {
@@ -120,10 +122,10 @@ describe("2. mRNA 서열 변환", () => {
         );
       cy.get("button.next-button").click();
 
-      let startValue = Math.floor(Math.random() * (1273 - 1 + 1)) + 1;
+      let startValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1;
       let endValue = Math.min(
         startValue + Math.floor(Math.random() * 100),
-        1273
+        1276
       );
 
       cy.intercept("POST", "/api/analysis/linearDesign").as(
@@ -149,7 +151,7 @@ describe("2. mRNA 서열 변환", () => {
   });
 
   it("2-1. Convert 버튼을 통해 데이터 전송 및 mRNA 디자인 탭으로 이동", () => {
-    cy.wait(20000); 
+    cy.wait(20000);
     cy.get(".nav-tabs")
       .contains("mRNA design")
       .should("not.have.class", "disabled");
