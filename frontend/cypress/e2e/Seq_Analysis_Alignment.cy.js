5d1b4f41389f94cee9f34dfaa2b37c865f77caf9
kky107102 <kky107102@ajou.ac.kr>
2024-10-25 21:55:57 +0900

    test: Terminal error 수정

--- frontend/cypress/e2e/Seq_Analysis_Alignment.cy.js
+++ frontend/cypress/e2e/Seq_Analysis_Alignment.cy.js
@@ -1,7 +1,7 @@
 
 // 서열 분석 페이지 및 서열 정렬 탭
 describe("1. 서열 정렬(alignment)", () => {
-  beforeEach(() => {
+  it("1-1. 서열 정렬 알고리즘실행하여 정렬된 데이터 시각화", () => {
     cy.signupAndLoginIfDuplicate(
       "testFName",
       "testLName",
@@ -11,43 +11,17 @@ describe("1. 서열 정렬(alignment)", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+    cy.inputSeqSetup();
 
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-    });
-  });
-
-  // 1-1. 서열 정렬 알고리즘실행하여 정렬된 데이터 시각화
-  it("1-1. 서열 정렬 알고리즘실행하여 정렬된 데이터 시각화", () => {
     cy.wait("@alignmentRequest", { timeout: 20000 }).then((interception) => {
       expect(interception.response.statusCode).to.eq(200);
-
       cy.get(".sequence-boxes").find(".sequence-box").should("exist");
     });
   });
 });
 
-describe("2. 불일치 구간(돌연변이) 색상 표시", () => {
-  beforeEach(() => {
+describe("2. 불일치 구간(돌연변이) 색상 표시", () => {    
+  it("2-1. 불일치 아미노산 탐지 및 색상 변화 적용", () => {
     cy.signupAndLoginIfDuplicate(
       "testFName",
       "testLName",
@@ -57,74 +31,25 @@ describe("2. 불일치 구간(돌연변이) 색상 표시", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+    cy.inputSeqSetup();
 
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-    });
-  });
-
-  // 2-1. 불일치 아미노산 탐지 및 색상 변화 적용
-  it("2-1. 불일치 아미노산 탐지 및 색상 변화 적용", () => {
     cy.get(".sequence-boxes").find(".sequence-box.gap.different").should("exist");
   });
 });
 
 describe("3. 유전체 바", () => {
-    beforeEach(() => {
-        cy.signupAndLoginIfDuplicate(
-          "testFName",
-          "testLName",
-          "testId",
-          "testPw",
-          "testPw"
-        );
-        cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
-        cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
-
-        // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-        cy.fixture("referenceId").then((referenceId) => {
-          // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-          cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-          cy.get("button.done-button").click();
-          cy.wait("@metadataRequest").then((interception) => {
-            expect(interception.response.statusCode).to.eq(200);
-
-            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-            cy.contains("Sequence ID").should("be.visible");
-            cy.contains("Name").should("be.visible");
-            cy.contains("Description").should("be.visible");
-            cy.contains("Length").should("be.visible");
-          });
-          cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-            .parent() // 부모 요소로 이동
-            .find("textarea") // 부모 요소 아래의 textarea를 찾음
-            .type(
-              "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-            );
-          cy.get("button.next-button").click();
-        });
-      });
-
   it("3-1. 유전체를 선택하여 해당하는 서열 표시", () => {
+    cy.signupAndLoginIfDuplicate(
+      "testFName",
+      "testLName",
+      "testId",
+      "testPw",
+      "testPw"
+    );
+    cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
+    cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+    cy.inputSeqSetup();
+
     // 변경 전 기본 옵션 값 확인
     cy.get('#region-select').should('have.value', 'ORF1ab');
 
@@ -141,7 +66,8 @@ describe("3. 유전체 바", () => {
   });
 
 describe("4. 도움말 아이콘", () => {
-  beforeEach(() => {
+  // Help 버튼 클릭 시 모달 창이 뜨는지 확인
+  it("4-1. 사용 설명서 안내", () => {
     cy.signupAndLoginIfDuplicate(
       "testFName",
       "testLName",
@@ -151,33 +77,8 @@ describe("4. 도움말 아이콘", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+    cy.inputSeqSetup();
 
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-    });
-  });
-
-  // Help 버튼 클릭 시 모달 창이 뜨는지 확인
-  it("4-1. 사용 설명서 안내", () => {
     // 도움말 버튼 클릭
     cy.get(".help-icon").click();
 
@@ -192,7 +93,8 @@ describe("4. 도움말 아이콘", () => {
 });
 
 describe("5. mRNA 변환", () => {
-  beforeEach(() => {
+  // Sequence Box 클릭 후 모달 창이 뜨는지 확인
+  it("5-1. 서열을 클릭하여 서열 변환 창 실행", () => {
     cy.signupAndLoginIfDuplicate(
       "testFName",
       "testLName",
@@ -202,32 +104,8 @@ describe("5. mRNA 변환", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
+    cy.inputSeqSetup();
 
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-    });
-  });
-  // Sequence Box 클릭 후 모달 창이 뜨는지 확인
-  it("5-1. 서열을 클릭하여 서열 변환 창 실행", () => {
     // Sequence Box 갯수 가져와서 랜덤하게 하나 클릭
     cy.get('.sequence-boxes').its('length').then((len) => {
       const randomIndex = Math.floor(Math.random() * len);
@@ -240,8 +118,9 @@ describe("5. mRNA 변환", () => {
 });
 
 
-describe("6. 히스토리 저장", () => {
-    beforeEach(() => {
+describe("6. 히스토리 저장", () => {  
+    // 히스토리 아이템 클릭 후 저장된 데이터 확인
+    it("6-1. 생성된 정렬 데이터 확인", () => {
       cy.signupAndLoginIfDuplicate(
         "testFName",
         "testLName",
@@ -251,32 +130,8 @@ describe("6. 히스토리 저장", () => {
       );
       cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
       cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
-  
-      // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-      cy.fixture("referenceId").then((referenceId) => {
-        cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-        cy.get("button.done-button").click();
-        cy.wait("@metadataRequest").then((interception) => {
-          expect(interception.response.statusCode).to.eq(200);
-  
-          // "Sequence ID", "Name", "Description", "Length" 확인
-          cy.contains("Sequence ID").should("be.visible");
-          cy.contains("Name").should("be.visible");
-          cy.contains("Description").should("be.visible");
-          cy.contains("Length").should("be.visible");
-        });
-        cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div 찾음
-          .parent() // 부모 요소로 이동
-          .find("textarea") // 부모 요소 아래의 textarea 찾음
-          .type(
-            "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-          );
-        cy.get("button.next-button").click();
-      });
-    });
-  
-    // 히스토리 아이템 클릭 후 저장된 데이터 확인
-    it("6-1. 생성된 정렬 데이터 확인", () => {
+      cy.inputSeqSetup();
+
       // 히스토리 아이템 첫 번째 요소 클릭
       cy.get(".history-list .history-item").first().click();
   
--- frontend/cypress/e2e/mRNA_Design_tab.cy.js
+++ frontend/cypress/e2e/mRNA_Design_tab.cy.js
@@ -9,69 +9,28 @@ describe("1. mRNA 시각화 및 정보 제공", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
-
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-
-      let startValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1;
-      let endValue = Math.min(
-        startValue + Math.floor(Math.random() * 100),
-        1276
-      );
-
-      cy.intercept("POST", "/api/analysis/linearDesign").as(
-        "linearDesignRequest"
-      );
-
-      cy.get(".sequence-boxes").eq(0).click();
-
-      cy.contains(".modal-content label", "Select Coding Sequence:")
-        .find("select")
-        .select("S");
-
-      cy.contains(".modal-content label", "Start Amino Acid Position:")
-        .find('input[type="number"]')
-        .type(startValue.toString());
-
-      cy.contains(".modal-content label", "End Amino Acid Position:")
-        .find('input[type="number"]')
-        .type(endValue.toString());
-
-      cy.get(".modal-next-button").click();
-    });
+    cy.intercept("POST", "/api/analysis/linearDesign").as("linearDesignRequest");
+    cy.LinearDesignConvert();
   });
 
   it("1-1. mRNA 2D 구조 시각화", () => {
-    cy.contains("mRNA Visualization").should("be.visible");
-    cy.get("#plotting-area").should("be.visible");
+    cy.wait('@linearDesignRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      cy.contains("mRNA Visualization").should("be.visible");
+      cy.get("#plotting-area").should("be.visible");
+    });
   });
 
   it("1-2. RNA 및 단백질 파라미터 제공", () => {
-    cy.contains("Protein Parameters").should("be.visible");
+    cy.wait('@linearDesignRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      cy.contains("Protein Parameters").should("be.visible");
+    });
   });
 });
 
 describe("2. 히스토리 저장", () => {
-  beforeEach(() => {
+  it("2-1. 생성된 mRNA 데이터 기록 저장", () => {
     cy.signupAndLoginIfDuplicate(
       "testFName",
       "testLName",
@@ -81,62 +40,13 @@ describe("2. 히스토리 저장", () => {
     );
     cy.intercept("POST", "/api/inputSeq/metadata").as("metadataRequest");
     cy.intercept("POST", "/api/inputSeq/alignment").as("alignmentRequest");
-
-    // NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
-    cy.fixture("referenceId").then((referenceId) => {
-      // 올바른 NCBI 레퍼런스 시퀀스 ID 입력 후 'Done' 버튼 클릭
-      cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-      cy.get("button.done-button").click();
-      cy.wait("@metadataRequest").then((interception) => {
-        expect(interception.response.statusCode).to.eq(200);
-
-        // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-        cy.contains("Sequence ID").should("be.visible");
-        cy.contains("Name").should("be.visible");
-        cy.contains("Description").should("be.visible");
-        cy.contains("Length").should("be.visible");
-      });
-      cy.contains("div.sequence-header", "Sequence1") // 'Sequence1' 텍스트가 포함된 div를 찾음
-        .parent() // 부모 요소로 이동
-        .find("textarea") // 부모 요소 아래의 textarea를 찾음
-        .type(
-          "ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC"
-        );
-      cy.get("button.next-button").click();
-
-      let startValue = Math.floor(Math.random() * (1276 - 1 + 1)) + 1;
-      let endValue = Math.min(
-        startValue + Math.floor(Math.random() * 100),
-        1276
-      );
-
-      cy.intercept("POST", "/api/analysis/linearDesign").as(
-        "linearDesignRequest"
-      );
-
-      cy.get(".sequence-boxes").eq(0).click();
-
-      cy.contains(".modal-content label", "Select Coding Sequence:")
-        .find("select")
-        .select("S");
-
-      cy.contains(".modal-content label", "Start Amino Acid Position:")
-        .find('input[type="number"]')
-        .type(startValue.toString());
-
-      cy.contains(".modal-content label", "End Amino Acid Position:")
-        .find('input[type="number"]')
-        .type(endValue.toString());
-
-      cy.get(".modal-next-button").click();
+    cy.intercept("POST", "/api/analysis/linearDesign").as("linearDesignRequest");
+    cy.LinearDesignConvert();
+    cy.wait('@linearDesignRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      cy.get(".history-list .history-item").first().click();
+      cy.contains("mRNA Visualization").should("be.visible");
+      cy.contains("Protein Parameters").should("be.visible");
     });
-  });
-
-  it("2-1. 생성된 mRNA 데이터 기록 저장", () => {
-    cy.get(".history-list .history-item").first().click();
-
-    cy.contains("mRNA Visualization").should("be.visible");
-
-    cy.contains("Protein Parameters").should("be.visible");
   });
 });
