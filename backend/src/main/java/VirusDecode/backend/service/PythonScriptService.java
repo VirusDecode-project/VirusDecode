c98cafc325099abad28eddf38807d27d65d3f714
GK <weeeeek2@naver.com>
2024-10-27 12:56:13 +0900

    chore: Divide bioinformatics code to root

--- backend/src/main/java/VirusDecode/backend/service/PythonScriptService.java
+++ backend/src/main/java/VirusDecode/backend/service/PythonScriptService.java
@@ -4,8 +4,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -16,7 +14,7 @@ import java.util.List;
 @Log4j2
 @Service
 public class PythonScriptService {
-    private static final String pythonScriptPath = Paths.get("").toAbsolutePath().resolve("virusdecode.py").normalize().toString();
+    private static final String pythonScriptPath = Paths.get("").toAbsolutePath().resolve("../bioinformatics/virusdecode.py").normalize().toString();
 
     // ProcessBuilder를 생성하는 메서드를 따로 분리하여 테스트에서 모의 가능하도록 설계
     protected ProcessBuilder createProcessBuilder(List<String> command) {
