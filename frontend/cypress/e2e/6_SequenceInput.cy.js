28f29d7693ff1d2c3db7dc753496e100010070b9
GK <weeeeek2@naver.com>
2024-10-29 00:58:41 +0900

    test: Update 6_SequenceInput.cy.js

--- frontend/cypress/e2e/6_SequenceInput.cy.js
+++ frontend/cypress/e2e/6_SequenceInput.cy.js
@@ -1,5 +1,11 @@
 // 서열 입력 페이지
 describe('1. 레퍼런스 서열 입력 테스트', () => {
+    let referenceSeqId;
+    before(() => {
+        cy.fixture('environment').then((environment) => {
+            referenceSeqId = environment.SARS_CoV_2_ID;
+        });
+    });
     beforeEach(() => {
         cy.signupAndLoginIfDuplicate('testFName', 'testLName', 'testId', 'testPw', 'testPw');
         cy.intercept('POST', '/api/inputSeq/metadata').as('metadataRequest');
@@ -31,26 +37,30 @@ describe('1. 레퍼런스 서열 입력 테스트', () => {
 
     // 1-2. Nucleotide ID 입력 성공 및 NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기
     it('1-2. NCBI로부터 ID 유효성 검사 및 메타데이터 가져오기', () => {
-        // NCBI 레퍼런스 시퀀스 ID 입력 필드에 NC_045512.2을 입력한다. ‘Done’ 버튼을 누른다.
-        cy.fixture('referenceId').then((referenceId) => {
-            //   cy.get('input[id="referenceSequenceId"]').type('NC_045512.2');
-            cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-            cy.get('button.done-button').click();
-            cy.wait('@metadataRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
 
-                // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-                cy.contains('Sequence ID').should('be.visible');
-                cy.contains('Name').should('be.visible');
-                cy.contains('Description').should('be.visible');
-                cy.contains('Length').should('be.visible');
-            });
-        })
+        //   cy.get('input[id="referenceSequenceId"]').type('NC_045512.2');
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+
+            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
+        });
     });
 });
 
 
 describe('2. 분석 서열 입력', () => {
+    let referenceSeqId;
+    before(() => {
+        cy.fixture('environment').then((environment) => {
+            referenceSeqId = environment.SARS_CoV_2_ID;
+        });
+    });
     beforeEach(() => {
         cy.signupAndLoginIfDuplicate('testFName', 'testLName', 'testId', 'testPw', 'testPw');
         cy.intercept('POST', '/api/inputSeq/metadata').as('metadataRequest');
@@ -59,7 +69,7 @@ describe('2. 분석 서열 입력', () => {
 
     // 2-1-1 FASTA 파일 업로드 실패(잘못된 파일 업로드)
     it('2-1-1, 2-3 FASTA 파일 업로드 실패 및 입력 형식 검증: 잘못된 형식의 파일을 업로드', () => {
-        const filePath = 'referenceId.json';
+        const filePath = 'environment.json';
         cy.get('input[type="file"]').attachFile(filePath);
 
         cy.get('.message-modal-content')
@@ -70,82 +80,76 @@ describe('2. 분석 서열 입력', () => {
 
     // 2-1-2 FASTA 파일 업로드 성공
     it('2-1-2 FASTA 파일 업로드 성공: 사용자로부터 하나의 FASTA 파일업로드', () => {
-        cy.fixture('referenceId').then((referenceId) => {
-            cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-            cy.get('button.done-button').click();
-            cy.wait('@metadataRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
-                // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-                cy.contains('Sequence ID').should('be.visible');
-                cy.contains('Name').should('be.visible');
-                cy.contains('Description').should('be.visible');
-                cy.contains('Length').should('be.visible');
-            })
-
-            const filePath = 'SARS_CoV_2/MT576556.1.spike.fasta'; // fixtures 폴더에 있는 파일 경로
-            // 파일을 input[type="file"] 요소에 업로드
-            cy.get('input[type="file"]').attachFile(filePath);
-
-            // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
-            cy.contains('MT576556.1.spike.fasta').should('be.visible');
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
         })
+
+        const filePath = 'SARS_CoV_2/MT576556.1.spike.fasta'; // fixtures 폴더에 있는 파일 경로
+        // 파일을 input[type="file"] 요소에 업로드
+        cy.get('input[type="file"]').attachFile(filePath);
+
+        // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
+        cy.contains('MT576556.1.spike.fasta').should('be.visible');
     });
 
     // 2-1-3 FASTA 파일 업로드 성공
     it('2-1-3 FASTA 파일 업로드 성공: 사용자로부터 여러 FASTA 파일업로드', () => {
-        cy.fixture('referenceId').then((referenceId) => {
-            cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-            cy.get('button.done-button').click();
-            cy.wait('@metadataRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
-                // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-                cy.contains('Sequence ID').should('be.visible');
-                cy.contains('Name').should('be.visible');
-                cy.contains('Description').should('be.visible');
-                cy.contains('Length').should('be.visible');
-            })
-
-            const filePath1 = 'SARS_CoV_2/MT576556.1.spike.fasta';
-            const filePath2 = 'SARS_CoV_2/MW642250.1.spike.fasta';
-            const filePath3 = 'SARS_CoV_2/OL672836.1.spike.fasta';
-            const filePath4 = 'SARS_CoV_2/OM958567.1.spike.fasta';
-            const filePath5 = 'SARS_CoV_2/OR240434.1.spike.fasta';
-            const filePath6 = 'SARS_CoV_2/PP346415.1.spike.fasta';
-
-            // 파일을 input[type="file"] 요소에 업로드
-            cy.get('input[type="file"]').attachFile(filePath1);
-            cy.get('input[type="file"]').attachFile(filePath2);
-            cy.get('input[type="file"]').attachFile(filePath3);
-            cy.get('input[type="file"]').attachFile(filePath4);
-            cy.get('input[type="file"]').attachFile(filePath5);
-            cy.get('input[type="file"]').attachFile(filePath6);
-
-            // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
-            cy.contains('MT576556.1.spike.fasta').should('be.visible');
-            cy.contains('MW642250.1.spike.fasta').should('be.visible');
-            cy.contains('OL672836.1.spike.fasta').should('be.visible');
-            cy.contains('OM958567.1.spike.fasta').should('be.visible');
-            cy.contains('OR240434.1.spike.fasta').should('be.visible');
-            cy.contains('PP346415.1.spike.fasta').should('be.visible');
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
         })
+
+        const filePath1 = 'SARS_CoV_2/MT576556.1.spike.fasta';
+        const filePath2 = 'SARS_CoV_2/MW642250.1.spike.fasta';
+        const filePath3 = 'SARS_CoV_2/OL672836.1.spike.fasta';
+        const filePath4 = 'SARS_CoV_2/OM958567.1.spike.fasta';
+        const filePath5 = 'SARS_CoV_2/OR240434.1.spike.fasta';
+        const filePath6 = 'SARS_CoV_2/PP346415.1.spike.fasta';
+
+        // 파일을 input[type="file"] 요소에 업로드
+        cy.get('input[type="file"]').attachFile(filePath1);
+        cy.get('input[type="file"]').attachFile(filePath2);
+        cy.get('input[type="file"]').attachFile(filePath3);
+        cy.get('input[type="file"]').attachFile(filePath4);
+        cy.get('input[type="file"]').attachFile(filePath5);
+        cy.get('input[type="file"]').attachFile(filePath6);
+
+        // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
+        cy.contains('MT576556.1.spike.fasta').should('be.visible');
+        cy.contains('MW642250.1.spike.fasta').should('be.visible');
+        cy.contains('OL672836.1.spike.fasta').should('be.visible');
+        cy.contains('OM958567.1.spike.fasta').should('be.visible');
+        cy.contains('OR240434.1.spike.fasta').should('be.visible');
+        cy.contains('PP346415.1.spike.fasta').should('be.visible');
     });
 
     // 2-2-1 염기서열(A,T,C,G) 입력 실패(잘못된 서열 입력)
     it('2-2-1, 2-3 염기서열(A,T,C,G) 입력 실패: 잘못된 형식의 시퀀스 입력', () => {
-        cy.fixture('referenceId').then((referenceId) => {
-            cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-            cy.get('button.done-button').click();
-            cy.wait('@metadataRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
-                cy.contains('Sequence ID').should('be.visible');
-                cy.contains('Name').should('be.visible');
-                cy.contains('Description').should('be.visible');
-                cy.contains('Length').should('be.visible');
-            })
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
         })
 
-        cy.get('textarea[placeholder="TAGCTAGCCGATCG....."]')
-            .type('안녕하세요');
+        cy.get('.w-100')
+            .type('wrongSequence');
 
         cy.get('button.next-button').click();
         cy.wait('@alignmentRequest').then((interception) => {
@@ -159,67 +163,106 @@ describe('2. 분석 서열 입력', () => {
         })
     });
 
+    it('2-2-2 염기서열(A,T,C,G) 입력 성공', () => {
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
+        })
+
+        cy.contains('div.sequence-header', 'Sequence1')  // 'Sequence1' 텍스트가 포함된 div를 찾음
+            .parent()  // 부모 요소로 이동
+            .find('textarea')  // 부모 요소 아래의 textarea를 찾음
+            .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
+
+        cy.get('button.add-sequence-button').click();
+
+        cy.contains('div.sequence-header', 'Sequence2')  // 'Sequence2' 텍스트가 포함된 div를 찾음
+            .parent()  // 부모 요소로 이동
+            .find('textarea')  // 부모 요소 아래의 textarea를 찾음
+            .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
+
+
+        cy.get('button.next-button').click();
+        cy.wait('@alignmentRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+
+            // sequence-chunk 안의 sequence 클래스가 1 + 업로드한 FASTA 파일 개수(1개)인지 확인
+            cy.get('.sequence-chunk').eq(0)  // 첫 번째 청크 선택
+                .find('.sequence')  // 그 안에서 sequence 클래스 요소 찾기
+                .should('have.length', 3);  // 예상 개수와 비교
+        })
+    });
 
 
 });
 
 
 describe('3. 유전체 분석 테스트', () => {
+    let referenceSeqId;
+    before(() => {
+        cy.fixture('environment').then((environment) => {
+            referenceSeqId = environment.SARS_CoV_2_ID;
+        });
+    });
     beforeEach(() => {
         cy.signupAndLoginIfDuplicate('testFName', 'testLName', 'testId', 'testPw', 'testPw');
         cy.intercept('POST', '/api/inputSeq/metadata').as('metadataRequest');
         cy.intercept('POST', '/api/inputSeq/alignment').as('alignmentRequest');
     });
     it('3-1 Next 버튼을 통해 데이터 전송 및 분석 페이지 이동', () => {
-        cy.fixture('referenceId').then((referenceId) => {
-            cy.get('input[id="referenceSequenceId"]').type(referenceId.SARS_CoV_2_ID);
-            cy.get('button.done-button').click();
-            cy.wait('@metadataRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
-                // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
-                cy.contains('Sequence ID').should('be.visible');
-                cy.contains('Name').should('be.visible');
-                cy.contains('Description').should('be.visible');
-                cy.contains('Length').should('be.visible');
-            })
+        cy.get('input[id="referenceSequenceId"]').type(referenceSeqId);
+        cy.get('button.done-button').click();
+        cy.wait('@metadataRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            // "Sequence ID", "Name", "Description", "Length"가 있는지 확인
+            cy.contains('Sequence ID').should('be.visible');
+            cy.contains('Name').should('be.visible');
+            cy.contains('Description').should('be.visible');
+            cy.contains('Length').should('be.visible');
+        })
 
-            const filePath1 = 'SARS_CoV_2/MT576556.1.spike.fasta';
-            const filePath2 = 'SARS_CoV_2/MW642250.1.spike.fasta';
+        const filePath1 = 'SARS_CoV_2/MT576556.1.spike.fasta';
+        const filePath2 = 'SARS_CoV_2/MW642250.1.spike.fasta';
 
-            // 파일을 input[type="file"] 요소에 업로드
-            cy.get('input[type="file"]').attachFile(filePath1);
-            cy.get('input[type="file"]').attachFile(filePath2);
+        // 파일을 input[type="file"] 요소에 업로드
+        cy.get('input[type="file"]').attachFile(filePath1);
+        cy.get('input[type="file"]').attachFile(filePath2);
 
-            // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
-            cy.contains('MT576556.1.spike.fasta').should('be.visible');
-            cy.contains('MW642250.1.spike.fasta').should('be.visible');
+        // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
+        cy.contains('MT576556.1.spike.fasta').should('be.visible');
+        cy.contains('MW642250.1.spike.fasta').should('be.visible');
 
-            // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
-            cy.contains('MT576556.1.spike.fasta').should('be.visible');
+        // 파일이 제대로 업로드 되었는지 확인 (예: 업로드 후 확인 메시지나 파일 이름이 표시되는지)
+        cy.contains('MT576556.1.spike.fasta').should('be.visible');
 
-            // 두 개의 시퀀스 입력
-            cy.contains('div.sequence-header', 'Sequence1')  // 'Sequence1' 텍스트가 포함된 div를 찾음
-                .parent()  // 부모 요소로 이동
-                .find('textarea')  // 부모 요소 아래의 textarea를 찾음
-                .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
+        // 두 개의 시퀀스 입력
+        cy.contains('div.sequence-header', 'Sequence1')  // 'Sequence1' 텍스트가 포함된 div를 찾음
+            .parent()  // 부모 요소로 이동
+            .find('textarea')  // 부모 요소 아래의 textarea를 찾음
+            .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
 
-            cy.get('button.add-sequence-button').click();
+        cy.get('button.add-sequence-button').click();
 
-            cy.contains('div.sequence-header', 'Sequence2')  // 'Sequence2' 텍스트가 포함된 div를 찾음
-                .parent()  // 부모 요소로 이동
-                .find('textarea')  // 부모 요소 아래의 textarea를 찾음
-                .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
+        cy.contains('div.sequence-header', 'Sequence2')  // 'Sequence2' 텍스트가 포함된 div를 찾음
+            .parent()  // 부모 요소로 이동
+            .find('textarea')  // 부모 요소 아래의 textarea를 찾음
+            .type('ATGTTTGTTTTTCTTGTTTTATTGCCACTAGTCTCTAGTCAGTGTGTTAATCTTACAACCAGAACTCAATTACCCCCTGCATACACTAATTCTTTCACACGTGGTGTTTATTACCCTGACAAAGTTTTCAGATCCTCAGTTTTACATTCAACTCAGGACTTGTTCTTACCTTTCTTTTCC');
 
 
-            cy.get('button.next-button').click();
-            cy.wait('@alignmentRequest').then((interception) => {
-                expect(interception.response.statusCode).to.eq(200);
+        cy.get('button.next-button').click();
+        cy.wait('@alignmentRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
 
-                // sequence-chunk 안의 sequence 클래스가 1 + 업로드한 FASTA 파일 개수(1개)인지 확인
-                cy.get('.sequence-chunk').eq(0)  // 첫 번째 청크 선택
-                    .find('.sequence')  // 그 안에서 sequence 클래스 요소 찾기
-                    .should('have.length', 5);  // 예상 개수와 비교
-            })
+            // sequence-chunk 안의 sequence 클래스가 1 + 업로드한 FASTA 파일 개수(1개)인지 확인
+            cy.get('.sequence-chunk').eq(0)  // 첫 번째 청크 선택
+                .find('.sequence')  // 그 안에서 sequence 클래스 요소 찾기
+                .should('have.length', 5);  // 예상 개수와 비교
         })
     });
 });
@@ -244,14 +287,16 @@ describe('4. 히스토리 생성 테스트', () => {
             });
         });
     }
+    before(() => {
+        cy.fixture('environment').then((environment) => {
+            referenceSeqId = environment.SARS_CoV_2_ID;
+        });
+    });
     beforeEach(() => {
         // 기본 URL로 애플리케이션에 접속
         cy.signupAndLoginIfDuplicate('testFName', 'testLName', 'testId', 'testPw', 'testPw');
         cy.intercept('POST', '/api/inputSeq/metadata').as('metadataRequest');
         cy.intercept('POST', '/api/inputSeq/alignment').as('alignmentRequest');
-        cy.fixture('referenceId').then((referenceId) => {
-            referenceSeqId = referenceId.SARS_CoV_2_ID;
-        });
     });
 
 
